d322201754077f6cd8fb969b9a25817c2c78d9a6
SzymonDrzazga <szydrzazga@gmail.com>
2023-10-28 20:21:56 +0200

    chore: cleaned code

--- Strona Internetowa/src/App.svelte
+++ Strona Internetowa/src/App.svelte
@@ -1,30 +1,14 @@
 <script>
-	import svelteLogo from "./assets/svelte.svg";
-	import viteLogo from "/vite.svg";
 	import Counter from "./lib/Counter.svelte";
 </script>
 
 <main>
-	<div>
-		<a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-			<img src={viteLogo} alt="Vite Logo" />
-		</a>
-		<a href="https://svelte.dev" target="_blank" rel="noreferrer">
-			<img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-		</a>
-	</div>
-	<h1>Vite + Svelte</h1>
+	<h1 class="font-black text-center text-3xl logo">Jesteśmy By Pamiętać</h1>
 
-	<div class="card">
+	<div class="text-center">
 		<Counter />
 	</div>
 
-	<p class="underline">
-		Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework
-		powered by Vite!
-	</p>
-
-	<p class="read-the-docs">Click on the Vite and Svelte logos to learn more</p>
 </main>
 
 <style>
@@ -37,10 +21,7 @@
 	.logo:hover {
 		filter: drop-shadow(0 0 2em #646cffaa);
 	}
-	.logo.svelte:hover {
-		filter: drop-shadow(0 0 2em #ff3e00aa);
-	}
-	.read-the-docs {
+	/* .read-the-docs {
 		color: #888;
-	}
+	} */
 </style>
