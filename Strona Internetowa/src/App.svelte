74b9f4e3ff1c93f2067b400986affae3a7b92b31
SevenPik <sevenpik1234@gmail.com>
2023-11-02 20:18:15 +0100

    Navbar zrobiony.

--- Strona Internetowa/src/App.svelte
+++ Strona Internetowa/src/App.svelte
@@ -3,7 +3,6 @@
 	import { Router } from "svelte-routing";
 	import Navbar from './lib/Navbar.svelte';
 	import Content from './lib/Content.svelte';
-
 	export let url = "";
 </script>
 
--- Strona Internetowa/src/lib/Navbar.svelte
+++ Strona Internetowa/src/lib/Navbar.svelte
@@ -2,7 +2,7 @@
 	import { Link } from "svelte-routing";
 </script>
 
-<nav class="flex flex-row justify-around mb-4">
+<nav class="flex flex-row justify-around mb-4 bg-DomCzerw p-6 text-2xl subpixel-antialiased font-bold text-slate-200">
     <Link to="/">Home</Link>
     <Link to="/library">Library</Link>
     <Link to="/news">News</Link>
