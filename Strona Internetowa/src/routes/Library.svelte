f33d3930826319cfef4dacab29dd7ca7fe1983d9
SzymonDrzazga <szydrzazga@gmail.com>
2023-11-07 16:58:20 +0100

    chore: cleaning code

--- Strona Internetowa/src/routes/Library.svelte
+++ Strona Internetowa/src/routes/Library.svelte
@@ -1,3 +1,10 @@
+<script>
+	import { mainColorText, mainColorBorder } from '../store.js';
+
+    mainColorText.set("text-white");
+    mainColorBorder.set("border-white");
+</script>
+
 <div class="text-4xl font-bold h-screen flex items-center justify-center text-white">
     <p>Library</p>
 </div>
