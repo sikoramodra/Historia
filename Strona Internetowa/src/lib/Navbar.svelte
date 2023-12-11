e6fe114520d42269aa9c75f3cde67931689fef25
SzymonDrzazga <szydrzazga@gmail.com>
2023-12-11 09:56:29 +0100

    fix: deleted graphics folder

--- Strona Internetowa/src/lib/Navbar.svelte
+++ Strona Internetowa/src/lib/Navbar.svelte
@@ -14,7 +14,7 @@
 <nav class="fixed top-0 left-0 right-0 z-[60] px-8 h-20 { NavColorText }">
 	<div class="font-semibold text-2xl flex flex-row items-center h-full">
 		<div class="w-1/5 pl-8">
-			<Link to="/" class="text-left text-5xl px-12 font-bold"><img src="" alt="Home" /></Link>
+			<Link to="/" class="text-left text-5xl px-12 font-bold"><img src="" alt="Home" /></Link> <!-- Logo poziom_ciemne tło_PNG / Logo poziom_jasne tło_PNG -->
 		</div>
 		<div class="w-3/5 text-left">
 			<Link to="/library">Library</Link>
