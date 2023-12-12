5d3f9c17c064537baf36a9f6e3f1d08409db3bbb
SzymonDrzazga <szydrzazga@gmail.com>
2023-12-12 07:28:56 +0100

    feat: added responisive design to NavBar

--- Strona Internetowa/src/lib/Navbar.svelte
+++ Strona Internetowa/src/lib/Navbar.svelte
@@ -12,18 +12,17 @@
 </script>
 
 <nav class="fixed top-0 left-0 right-0 z-[60] px-8 h-20 { NavColorText }">
-	<div class="font-semibold text-2xl flex flex-row items-center h-full">
-		<div class="w-1/5 pl-8">
-			<Link to="/" class="text-left text-5xl px-12 font-bold"><img src="" alt="Home" /></Link> <!-- Logo poziom_ciemne tło_PNG / Logo poziom_jasne tło_PNG -->
+	<div class="font-semibold sm:text-base md:text-lg lg:text-xl xl:text-2xl 2xl:text-4xl text-sm flex flex-row items-center h-full">
+		<div class="w-1/5 min-w-fit sm:pl-6 md:pl-8 pl-4">
+			<Link to="/" class="text-left text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-8xl px-12 font-bold"><img src="" alt="Home" /></Link> <!-- Logo poziom_ciemne tło_PNG / Logo poziom_jasne tło_PNG -->
 		</div>
-		<div class="w-3/5 text-left">
-			<Link to="/library">Library</Link>
-			<Link to="/projects" class="w-3/5 mx-12">Projects</Link>
+		<div class="w-3/5 min-w-fit">
+			<Link to="/library" class="w-1/2 mx-8">Library</Link>
+			<Link to="/projects" class="w-1/2">Projects</Link>
 		</div>
-		<div class="w-1/5 text-right">
-			<Link to="/search-site" class="border-2 { NavColorBorder } p-2 rounded-lg"
-				>Search Site <i class="fa-solid fa-magnifying-glass" /></Link
-			>
+		<div class="w-1/5 text-right min-w-fit">
+			<Link to="/search-site" class="border-2 p-2 rounded-lg { NavColorBorder }"
+				>Search Site <i class="fa-solid fa-magnifying-glass" /></Link>
 		</div>
 	</div>
 </nav>
