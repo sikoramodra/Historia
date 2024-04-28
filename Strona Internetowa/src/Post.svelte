574ac31b58b5b6cf0c647c31785643cfd15a35f5
SevenPik <sevenpik1234@gmail.com>
2024-04-28 14:57:06 +0200

    feat: Article working 3 day sum

---
+++ Strona Internetowa/src/Post.svelte
@@ -0,0 +1,46 @@
+<script>
+  import { onMount, onDestroy } from 'svelte';
+  import { Router, Link } from 'svelte-routing';
+  import Jogurt from '../src/routes/post/jogurt.svelte'; // Importuj Jogurt.svelte
+  import Czekolada from '../src/routes/post/czekolada.svelte';
+  import Navbar from './lib/sections/Navbar.svelte';
+
+  // Ustawienie początkowej zawartości dla posta
+  let PostComponent = null;
+
+  // Funkcja do ładowania komponentu w zależności od ścieżki
+  const loadComponent = (path) => {
+    switch(path) {
+      case "/library/jogurt":
+        PostComponent = Jogurt;
+        break;
+      // Możesz dodać obsługę innych ścieżek tutaj
+      case "/library/czekolada":
+        PostComponent = Czekolada;
+        break;
+   
+      default:
+        PostComponent = null;
+    }
+  };
+
+  // Użycie onMount, aby sprawdzić ścieżkę podczas montowania komponentu
+  onMount(() => {
+    loadComponent(window.location.pathname);
+  });
+
+  // Użyj Router, aby wyświetlić odpowiedni komponent w zależności od ścieżki
+  // Ten fragment pozwala na dynamiczne ładowanie komponentów w zależności od ścieżki
+  // Jeśli ścieżka pasuje do któregoś z przypadków, zostanie załadowany odpowiedni komponent
+</script>
+
+<Router>
+  {#if PostComponent !== null}
+    <Navbar/>
+    <PostComponent/>
+    
+  {:else}
+    <p>No content to display</p>
+  {/if}
+</Router>
+ 
